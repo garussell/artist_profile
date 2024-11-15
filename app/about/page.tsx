1f509e4bfcad02752c466e186336002fd78c0d51
Allen Russell <garyrusselljr@gmail.com>
2024-11-15 08:04:24 -0700

    add top section of about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,34 @@
-
+import Image from 'next/image';
+import profilePic from '../../public/profilePic.png'; // Adjust the path to your image file
 
 export default function About() {
   return (
-    <h1>About</h1>
-  )
+    <section className="min-h-screen flex flex-col">
+      <div className="mx-auto p-4 w-full">
+        <div className="prose w-full max-w-none grid grid-cols-1 md:grid-cols-2 gap-100">
+          <div className="flex flex-col justify-center">
+            <h3 className="italic mb-4">My name is</h3>
+            <h1 className="mb-6">
+              Gary Allen
+            </h1>
+            <h1 className="mb-6">
+              Russell Jr
+            </h1>
+            <h3 className="italic">
+              My friends call me Allen
+            </h3>
+          </div>
+          <div className="flex">
+            <Image 
+              src={profilePic} 
+              alt="Allen Russell" 
+              className=""
+              // width={500}
+              // height={500}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
